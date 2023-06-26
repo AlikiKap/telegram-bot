2d397ff582f35bccd3d8a61a4133b8832037182d
Aliki Kapasakalidi <aliki02.kapasakalidi@gmail.com>
2023-06-26 06:37:57 +0300

    inline buttons were created

--- main.py
+++ main.py
@@ -1,14 +1,18 @@
 from aiogram import Bot, Dispatcher, executor, types
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton,  InlineKeyboardMarkup, InlineKeyboardButton
+
 API_TOKEN = '6261406455:AAE8EQPwyHFtI7uX5jw8C9umRwXSOcnyq8o'
 bot = Bot(token=API_TOKEN)
 
 dp = Dispatcher(bot)
+
+urlkb = InlineKeyboardMarkup(row_width=1)
+urlButton = InlineKeyboardButton(text="Записаться на приём",url='http://www.example.com/')
+urlButton2 = InlineKeyboardButton(text="Оплатить",url='http://www.example.com/')
+urlkb.add(urlButton,urlButton2)
 @dp.message_handler(commands=['start']) 
-async def send_welcome(message: types.Message):
-    await message.reply("Здравствуйте! Добро пожвловать в чат-бот врача Капасакалиди Дианы Фердинандовны. Здесь вы можете записаться на приём.")
-@dp.message_handler()
-async def echo(message: types.Message):
-   await message.answer(message.text)
- 
+async def url_command(message:types.Message):
+    await message.answer("Здравствуйте! Добро пожаловать в чат-бот врача Капасакалиди Дианы Фердинандовны. Здесь вы можете записаться на приём.", reply_markup=urlkb)
+
 if __name__ == '__main__':
    executor.start_polling(dp, skip_updates=True)
